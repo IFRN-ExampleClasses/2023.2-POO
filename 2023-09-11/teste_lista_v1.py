d8c76351622b30972e38126164c7e3c6bb7724eb
charles-freitas <ccmfreitas@gmail.com>
2023-09-11 14:07:03 -0300

    Implementando a Classe Lista com vários Métodos de Ordenação

---
+++ 2023-09-11/teste_lista_v1.py
@@ -0,0 +1,9 @@
+from class_Lista import Lista
+
+#lstTeste = Ordena('valores_nao_ordenado.txt')
+#lstTeste = Ordena(10, 20, 100)
+#lstTeste = Ordena(100)
+
+lstTeste = Lista(10, 20, 100)
+
+print(lstTeste.ListaValores)
