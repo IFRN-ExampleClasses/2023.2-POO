c7219b7aa5b0d377e9b47571e32d98b20b458be3
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2023-10-06 19:05:11 -0300

    Exemplo de Herança

---
+++ 2023-09-25/class_Aluno.py
@@ -0,0 +1,29 @@
+from class_Pessoa import Pessoa
+
+class Aluno(Pessoa):
+   
+    # --------------------------------------------------------------------------------
+    # Declarando os atributos
+    __matricula = None
+    __codcurso  = None
+    __campus    = None
+    __pessoa    = None
+
+    # --------------------------------------------------------------------------------
+    # Obtendo os atributos
+    @property
+    def dadosAcademicos(self):
+        return self.__pessoa.dados
+
+    # --------------------------------------------------------------------------------
+    def __init__(self, cpf: str, nome: str, email: str, matricula: str, codcurso: str, campus: str):
+        self.__matricula = matricula
+        self.__codcurso  = codcurso
+        self.__campus    = campus
+        super().__init__(cpf, nome, email)
+
+        self.__pessoa.dados[cpf][self.__matricula] = {
+            'matriculaAluno': self.__matricula,
+            'codCurso': self.__codcurso,
+            'campus': self.__campus
+        }
